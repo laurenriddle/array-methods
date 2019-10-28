c5cf8248c08746f9ab6445b160bcbcca992b754d
Lauren Riddle <lari7132@gmail.com>
2019-10-28 11:34:20 -0500

    filtered results and only returned the NY results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -114,16 +114,29 @@ const businesses = [
 const outEl = document.querySelector("#output")
 outEl.innerHTML = "<h1>Active Businesses</h1>"
 
-businesses.forEach(business => {
-    const zipcodeKey = "addressZipCode"
-    outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-    <section>
-      ${business.addressCity}, ${business["addressStateCode"]}, ${business[zipcodeKey]}
-    </section>
-  `
-    outEl.innerHTML += "<hr/>"
-});
+// businesses.forEach(business => {
+//     const zipcodeKey = "addressZipCode"
+//     outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//       ${business.addressCity}, ${business["addressStateCode"]}, ${business[zipcodeKey]}
+//     </section>
+//   `
+//     outEl.innerHTML += "<hr/>"
+// });
+
+// Array to contain all the New York businesses
+const newYorkBusinesses = businesses.filter(business => {
+    let inNewYork = false
+  
+    if (business.addressStateCode === "NY") {
+        inNewYork = true
+    }
+  
+    return inNewYork
+  })
+
+  console.log(newYorkBusinesses)
